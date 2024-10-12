470116b15fa72eeccac8010ded87e44a410f0205
BruleCool1 <152447228+BruleCool1@users.noreply.github.com>
2024-10-12 14:24:22 -0400

    Update your-shell-script.sh

--- api/your-shell-script.sh
+++ api/your-shell-script.sh
@@ -1,1 +1,1 @@
-chromium-browser --no-sandbox --disable-gpu --disable-dev-shm-usage --aggressive-cache-discard --disk-cache-size=0 --media-cache-size=0 --disable-extensions | sed 's/^\(.*\)$/<p>\1<\/p>/g' > output.html
+chromium-browser --no-sandbox --disable-gpu --disable-dev-shm-usage --aggressive-cache-discard --disk-cache-size=0 --media-cache-size=0 --disable-extensions | sed 's/^\(.*\)$/<p>\1<\/p>/g' > index.html
